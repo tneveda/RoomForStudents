8e81735da4b20cee6b377fde9dbf6d0f99745f84
AnaMendes <anam41065@gmail.com>
2022-05-04 17:29:39 +0100

    login - design + cod

--- app/src/main/java/ecgm/com/roomforstudents/MainActivity.kt
+++ app/src/main/java/ecgm/com/roomforstudents/MainActivity.kt
@@ -1,12 +1,12 @@
 package ecgm.com.roomforstudents
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+    }
 }
