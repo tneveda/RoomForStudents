771c24ed6d0dc474be9fe36ca43ddd7c41073ce9
tneveda <tiagoneveda@gmail.com>
2022-05-04 15:19:43 +0100

    1º Commit

---
+++ app/src/main/java/ecgm/com/roomforstudents/MainActivity.kt
@@ -0,0 +1,11 @@
+package ecgm.com.roomforstudents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
