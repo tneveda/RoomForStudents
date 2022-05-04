7c82abefb83a1b1d52ac51e586b8f356a86740b9
tneveda <tiagoneveda@gmail.com>
2022-05-04 18:16:02 +0100

    Correção de Conflitos

--- app/src/main/java/ecgm/com/roomforstudents/SplashScreen.kt
+++ app/src/main/java/ecgm/com/roomforstudents/SplashScreen.kt
@@ -14,7 +14,7 @@ class SplashScreen : AppCompatActivity() {
         supportActionBar?.hide()
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
+            val intent = Intent(this,ListaAnuncios::class.java)
             startActivity(intent)
             finish()
         }, 2000)
