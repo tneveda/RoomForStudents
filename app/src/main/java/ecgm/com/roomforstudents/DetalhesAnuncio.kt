4621cbcbddd146aeec745c7019d59cb9e7f50f31
AnaMendes <anam41065@gmail.com>
2022-05-18 16:35:37 +0100

    detalhes anuncio

---
+++ app/src/main/java/ecgm/com/roomforstudents/DetalhesAnuncio.kt
@@ -0,0 +1,11 @@
+package ecgm.com.roomforstudents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesAnuncio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_anuncio)
+    }
+}
