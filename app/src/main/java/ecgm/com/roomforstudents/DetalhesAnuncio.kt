2cf9f5116ed26b66b0292b7e32165efe10ec2161
tneveda <tiagoneveda@gmail.com>
2022-05-19 16:34:22 +0100

    Alterações no layout

--- app/src/main/java/ecgm/com/roomforstudents/DetalhesAnuncio.kt
+++ app/src/main/java/ecgm/com/roomforstudents/DetalhesAnuncio.kt
@@ -1,9 +1,28 @@
 package ecgm.com.roomforstudents
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import ecgm.com.roomforstudents.api.Anuncio
 
 class DetalhesAnuncio : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+    private lateinit var  anuncios: List<Anuncio>
+    private lateinit var shared_preferences: SharedPreferences
+    private lateinit var moradaView: TextView
+    private lateinit var priceView: TextView
+    private lateinit var numberView: TextView
+    private lateinit var quartosView: TextView
+    private lateinit var casaBanhoView: TextView
+    private lateinit var movelView: TextView
+    private lateinit var contactoView: TextView
+    private lateinit var observacoesView: TextView
+    private lateinit var qrcodeView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes_anuncio)
--- app/src/main/java/ecgm/com/roomforstudents/DetalhesAnuncioLogado.kt
+++ app/src/main/java/ecgm/com/roomforstudents/DetalhesAnuncioLogado.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DetalhesAnuncioLogado : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes_anuncio_logado)
