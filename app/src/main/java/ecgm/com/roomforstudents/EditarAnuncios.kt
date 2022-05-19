c2b76725e028f15026e14177fcfc9ac33bf3a0d4
AnaMendes <anam41065@gmail.com>
2022-05-19 23:28:39 +0100

    update editar anuncios - logado

---
+++ app/src/main/java/ecgm/com/roomforstudents/EditarAnuncios.kt
@@ -0,0 +1,14 @@
+package ecgm.com.roomforstudents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class EditarAnuncios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_anuncios)
+    }
+
+    fun inserir(view: View) {}
+}
